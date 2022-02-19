c2ef63886396afbe90311bab792f00eb1a2153c7
Roderick <enuke4@gmail.com>
2022-02-19 14:09:56 -0600

    server is set up

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,13 @@
 //Entry Point of the App
 
-console.log('MERN App');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+  res.status(200).json({ message: 'Starts here...' });
+});
+
+app.listen(port, () => console.log(`Server is running on PORT ${port}`));
