ff5d3e21142dee27c9d615064b03861ef5d83096
Roderick <enuke4@gmail.com>
2022-02-21 11:47:54 -0600

    create the register user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,10 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+
+
 /**
  * Register a new user
  * POST/api/users
@@ -6,9 +13,49 @@
  * @access Public
  */
 
-const registerUser = (req, res) => {
-  res.json({ message: 'Register User' })
-}
+const registerUser = asyncHandler(async (req, res) => {
+
+  const { name, email, password } = req.body;
+
+  //Validate that all fields have the information
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please enter all fields')
+  }
+
+  //Validate if user already exists
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw newError('User already exist')
+  }
+
+  //Hash the password using genSalt & hash method of bcrypt
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
+
+  //Confirmation when the user is created
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user information')
+  }
+
+  // res.json({ message: 'Register User' })
+})
 
 /**
  * Authenticate a user
@@ -18,9 +65,9 @@ const registerUser = (req, res) => {
  * @access Public
  */
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: 'Login User' })
-}
+})
 
 /**
  * Get a user data
@@ -30,8 +77,8 @@ const loginUser = (req, res) => {
  * @access Public
  */
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data' })
-}
+})
 
 module.exports = { registerUser, loginUser, getMe, };
