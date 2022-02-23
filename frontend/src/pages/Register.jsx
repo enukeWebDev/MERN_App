c5ebf78e51994bb36b24cc97516b4a7e1c8fa610
Roderick <enuke4@gmail.com>
2022-02-23 10:33:49 -0600

    start the register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 function Register() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData;
+
   return (
     <div>Register</div>
   )
---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+// module.exports = {
+//   resolve: {
+//     fallback: { url: require.resolve("url/") }
+//   }
+// }
+
+// module.exports = {
+//   resolve: {
+//     fallback: { path: require.resolve("path-browserify") }
+//   }
+// }
+
+// module.exports = {
+//   resolve: {
+//     fallback: { zlib: require.resolve("browserify-zlib") }
+//   }
+// }
+
+// module.exports = {
+//   resolve: {
+//     fallback: { crypto: require.resolve("crypto-browserify") }
+//   }
+// }
+
+// module.exports = {
+//   resolve: {
+//     // fallback: { stream: require.resolve("stream-browserify") }
+//     fallback: { "stream": false }
+//   }
+// }
+
+// module.exports = {
+//   resolve: {
+//     fallback: { "path": false }
+//   }
+// }
