698cebfd63e2d0eb9285f4474d55d40efbc1c3eb
Roderick <enuke4@gmail.com>
2022-02-23 10:58:29 -0600

    complete registration from layout - working state

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -29,13 +29,53 @@ function Register() {
       <section>
         <form>
           <div className="form-group">
-            <input type="text"
+            <input
+              type="text"
               className="form-control"
               id="name"
               name="name"
               value={name}
               placeholder="Please Enter Your Name"
               onChange={onChange} />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Please Enter Your Email"
+              onChange={onChange} />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Please Enter Your Password"
+              onChange={onChange} />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Please Confirm Your Password"
+              onChange={onChange} />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
           </div>
         </form>
       </section>
